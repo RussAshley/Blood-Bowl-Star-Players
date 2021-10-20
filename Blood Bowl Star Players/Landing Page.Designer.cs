d4a157148dd7e6742040aa59083578a162e2aa0e
ninjabredman <russellashley@hotmail.co.uk>
2021-10-20 20:20:30 +0100

    Add project files.

---
+++ Blood Bowl Star Players/Landing Page.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace Blood_Bowl_Star_Players
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.starPlayerNametextBox = new System.Windows.Forms.TextBox();
+            this.CosttextBox = new System.Windows.Forms.TextBox();
+            this.MAtextBox = new System.Windows.Forms.TextBox();
+            this.STtextBox = new System.Windows.Forms.TextBox();
+            this.AGtextBox = new System.Windows.Forms.TextBox();
+            this.PAtextBox = new System.Windows.Forms.TextBox();
+            this.AVtextBox = new System.Windows.Forms.TextBox();
+            this.SkillstextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ST = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.specialRulestextBox = new System.Windows.Forms.TextBox();
+            this.ImagerichTextBox = new System.Windows.Forms.RichTextBox();
+            this.LeaguestextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // starPlayerNametextBox
+            // 
+            this.starPlayerNametextBox.Location = new System.Drawing.Point(65, 47);
+            this.starPlayerNametextBox.Name = "starPlayerNametextBox";
+            this.starPlayerNametextBox.PlaceholderText = "Name";
+            this.starPlayerNametextBox.Size = new System.Drawing.Size(285, 23);
+            this.starPlayerNametextBox.TabIndex = 0;
+            // 
+            // CosttextBox
+            // 
+            this.CosttextBox.Location = new System.Drawing.Point(369, 47);
+            this.CosttextBox.Name = "CosttextBox";
+            this.CosttextBox.PlaceholderText = "Cost";
+            this.CosttextBox.Size = new System.Drawing.Size(100, 23);
+            this.CosttextBox.TabIndex = 1;
+            // 
+            // MAtextBox
+            // 
+            this.MAtextBox.Location = new System.Drawing.Point(65, 119);
+            this.MAtextBox.Name = "MAtextBox";
+            this.MAtextBox.PlaceholderText = "MA";
+            this.MAtextBox.Size = new System.Drawing.Size(58, 23);
+            this.MAtextBox.TabIndex = 2;
+            // 
+            // STtextBox
+            // 
+            this.STtextBox.Location = new System.Drawing.Point(129, 119);
+            this.STtextBox.Name = "STtextBox";
+            this.STtextBox.Size = new System.Drawing.Size(58, 23);
+            this.STtextBox.TabIndex = 3;
+            // 
+            // AGtextBox
+            // 
+            this.AGtextBox.Location = new System.Drawing.Point(193, 119);
+            this.AGtextBox.Name = "AGtextBox";
+            this.AGtextBox.Size = new System.Drawing.Size(58, 23);
+            this.AGtextBox.TabIndex = 4;
+            // 
+            // PAtextBox
+            // 
+            this.PAtextBox.Location = new System.Drawing.Point(257, 119);
+            this.PAtextBox.Name = "PAtextBox";
+            this.PAtextBox.Size = new System.Drawing.Size(58, 23);
+            this.PAtextBox.TabIndex = 5;
+            // 
+            // AVtextBox
+            // 
+            this.AVtextBox.Location = new System.Drawing.Point(321, 119);
+            this.AVtextBox.Name = "AVtextBox";
+            this.AVtextBox.Size = new System.Drawing.Size(58, 23);
+            this.AVtextBox.TabIndex = 6;
+            // 
+            // SkillstextBox
+            // 
+            this.SkillstextBox.Location = new System.Drawing.Point(385, 119);
+            this.SkillstextBox.Multiline = true;
+            this.SkillstextBox.Name = "SkillstextBox";
+            this.SkillstextBox.Size = new System.Drawing.Size(240, 23);
+            this.SkillstextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "MA";
+            // 
+            // ST
+            // 
+            this.ST.AutoSize = true;
+            this.ST.Location = new System.Drawing.Point(148, 101);
+            this.ST.Name = "ST";
+            this.ST.Size = new System.Drawing.Size(19, 15);
+            this.ST.TabIndex = 10;
+            this.ST.Text = "ST";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "AG";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(272, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "PA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(342, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "AV";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(436, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Skills";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(475, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Cost (K)";
+            // 
+            // specialRulestextBox
+            // 
+            this.specialRulestextBox.Location = new System.Drawing.Point(65, 158);
+            this.specialRulestextBox.Multiline = true;
+            this.specialRulestextBox.Name = "specialRulestextBox";
+            this.specialRulestextBox.PlaceholderText = "Special Rules";
+            this.specialRulestextBox.Size = new System.Drawing.Size(285, 185);
+            this.specialRulestextBox.TabIndex = 16;
+            // 
+            // ImagerichTextBox
+            // 
+            this.ImagerichTextBox.Location = new System.Drawing.Point(385, 158);
+            this.ImagerichTextBox.Name = "ImagerichTextBox";
+            this.ImagerichTextBox.Size = new System.Drawing.Size(240, 271);
+            this.ImagerichTextBox.TabIndex = 17;
+            this.ImagerichTextBox.Text = "";
+            // 
+            // LeaguestextBox
+            // 
+            this.LeaguestextBox.Location = new System.Drawing.Point(65, 374);
+            this.LeaguestextBox.Multiline = true;
+            this.LeaguestextBox.Name = "LeaguestextBox";
+            this.LeaguestextBox.PlaceholderText = "Leagues";
+            this.LeaguestextBox.Size = new System.Drawing.Size(285, 39);
+            this.LeaguestextBox.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 450);
+            this.Controls.Add(this.LeaguestextBox);
+            this.Controls.Add(this.ImagerichTextBox);
+            this.Controls.Add(this.specialRulestextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ST);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SkillstextBox);
+            this.Controls.Add(this.AVtextBox);
+            this.Controls.Add(this.PAtextBox);
+            this.Controls.Add(this.AGtextBox);
+            this.Controls.Add(this.STtextBox);
+            this.Controls.Add(this.MAtextBox);
+            this.Controls.Add(this.CosttextBox);
+            this.Controls.Add(this.starPlayerNametextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox starPlayerNametextBox;
+        private System.Windows.Forms.TextBox CosttextBox;
+        private System.Windows.Forms.TextBox MAtextBox;
+        private System.Windows.Forms.TextBox STtextBox;
+        private System.Windows.Forms.TextBox AGtextBox;
+        private System.Windows.Forms.TextBox PAtextBox;
+        private System.Windows.Forms.TextBox AVtextBox;
+        private System.Windows.Forms.TextBox SkillstextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ST;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox specialRulestextBox;
+        private System.Windows.Forms.RichTextBox ImagerichTextBox;
+        private System.Windows.Forms.TextBox LeaguestextBox;
+    }
+}
+
---
+++ Blood Bowl Star Players/StarPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blood_Bowl_Star_Players
+{
+    class StarPlayer
+    {
+
+        private string starPlayerName;
+        private int movementAllowance;
+        private int strength;
+        private int agility;
+        private int passingAbility;
+        private int armourValue;
+        private string skills;
+        private string halflingThimbleCup;
+        private string lustrianSuperLeague;
+        private string oldWorldClassic;
+        private string sylvanianSpotlight;
+        private string elvenKingdomsLeague;
+        private string worldsEdgeSuperleague;
+        private string badlandsBrawl;
+        private string underworldChallenge;
+        private string favouredOf;
+        private int cost;
+        private string starPlayerImage;
+
+        public StarPlayer(string starPlayerName, int movementAllowance, int strength, int agility, int passingAbility, int armourValue, string skills, string halflingThimbleCup, string lustrianSuperLeague, string oldWorldClassic, string sylvanianSpotlight, string elvenKingdomsLeague, string worldsEdgeSuperleague, string badlandsBrawl, string underworldChallenge, string favouredOf, int cost, string starPlayerImage)
+        {
+            this.StarPlayerName = starPlayerName;
+            this.MovementAllowance = movementAllowance;
+            this.Strength = strength;
+            this.Agility = agility;
+            this.PassingAbility = passingAbility;
+            this.ArmourValue = armourValue;
+            this.Skills = skills;
+            this.HalflingThimbleCup = halflingThimbleCup;
+            this.LustrianSuperLeague = lustrianSuperLeague;
+            this.OldWorldClassic = oldWorldClassic;
+            this.SylvanianSpotlight = sylvanianSpotlight;
+            this.ElvenKingdomsLeague = elvenKingdomsLeague;
+            this.WorldsEdgeSuperleague = worldsEdgeSuperleague;
+            this.BadlandsBrawl = badlandsBrawl;
+            this.UnderworldChallenge = underworldChallenge;
+            this.FavouredOf = favouredOf;
+            this.Cost = cost;
+            this.StarPlayerImage = starPlayerImage;
+        }
+
+        public string StarPlayerName { get => starPlayerName; set => starPlayerName = value; }
+        public int MovementAllowance { get => movementAllowance; set => movementAllowance = value; }
+        public int Strength { get => strength; set => strength = value; }
+        public int Agility { get => agility; set => agility = value; }
+        public int PassingAbility { get => passingAbility; set => passingAbility = value; }
+        public int ArmourValue { get => armourValue; set => armourValue = value; }
+        public string Skills { get => skills; set => skills = value; }
+        public string HalflingThimbleCup { get => halflingThimbleCup; set => halflingThimbleCup = value; }
+        public string LustrianSuperLeague { get => lustrianSuperLeague; set => lustrianSuperLeague = value; }
+        public string OldWorldClassic { get => oldWorldClassic; set => oldWorldClassic = value; }
+        public string SylvanianSpotlight { get => sylvanianSpotlight; set => sylvanianSpotlight = value; }
+        public string ElvenKingdomsLeague { get => elvenKingdomsLeague; set => elvenKingdomsLeague = value; }
+        public string WorldsEdgeSuperleague { get => worldsEdgeSuperleague; set => worldsEdgeSuperleague = value; }
+        public string BadlandsBrawl { get => badlandsBrawl; set => badlandsBrawl = value; }
+        public string UnderworldChallenge { get => underworldChallenge; set => underworldChallenge = value; }
+        public string FavouredOf { get => favouredOf; set => favouredOf = value; }
+        public int Cost { get => cost; set => cost = value; }
+        public string StarPlayerImage { get => starPlayerImage; set => starPlayerImage = value; }
+    }
+}
